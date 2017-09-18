924cc7e3d00ce620a0623d50985e902dffc47049
Mehedy Al Hasan <mdmehedy302@gmail.com>
2017-09-18 11:05:22 +0600

    Part1 compleated

--- Part1.java
+++ Part1.java
@@ -2,6 +2,18 @@
 public class Part1
 {
     
+      public static void main(String[] args)
+      
+{    Part2 ob1 = new Part2();
+    
+    ob1.testSimpleGene();
+    
+    
+    
+    
+    
+}
+    
    public  String findSimpleGene(String dna)
     {
         String result  = "";
@@ -28,8 +40,58 @@ public class Part1
        
     }
     
-    
-    
-    
-    
 }
+    
+ class Part2
+    {
+       
+        
+        
+        public  String findSimpleGene(String dna,int startCodon,int stopCodon)
+    {
+        String result  = "";
+         
+        
+        if((stopCodon - startCodon) % 3 == 0)
+            result = dna.substring(startCodon , stopCodon + 3);
+        
+        return result;
+        
+    }
+    
+    
+       public void testSimpleGene()
+   {
+       String dna = "atgtaa"; 
+       
+       int startIndex =dna.toUpperCase(). indexOf("ATG");
+       int stopIndex = dna.toUpperCase().indexOf("TAA",startIndex);
+     
+       System.out.println("STRAND :"+dna);
+       System.out.println("GENE :"+findSimpleGene(dna , startIndex , stopIndex));
+       
+       
+       
+    } 
+    
+    public int startCodon(String dna)
+       {
+          return dna.indexOf("ATG"); 
+          
+        }
+        
+    public int stopCodon(String dna,int startIndex)
+       {
+          return dna.indexOf("TAA", startIndex + 3); 
+          
+        }
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    }
